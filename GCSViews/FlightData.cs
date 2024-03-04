5f7433e3c8dc9a3a145d6b888078cd3589ae4a70
Bob Long <robertlong13@gmail.com>
2024-03-04 08:57:37 -0500

    GStreamer: properly stop thread

--- GCSViews/FlightData.cs
+++ GCSViews/FlightData.cs
@@ -3071,8 +3071,6 @@ namespace MissionPlanner.GCSViews
 
         private void HereLinkVideoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            hudGStreamer.Stop();
-
             string ipaddr = "192.168.43.1";
 
             if (Settings.Instance["herelinkip"] != null)
@@ -3098,7 +3096,7 @@ namespace MissionPlanner.GCSViews
                 }
             }
 
-            GCSViews.FlightData.hudGStreamer.StartA(url);
+            GCSViews.FlightData.hudGStreamer.Start(url);
         }
 
         private void hud_UserItem(object sender, EventArgs e)
@@ -4704,8 +4702,6 @@ namespace MissionPlanner.GCSViews
             {
                 Settings.Instance["gstreamer_url"] = url;
 
-                hudGStreamer.Stop();
-
                 GStreamer.GstLaunch = GStreamer.LookForGstreamer();
 
                 if (!GStreamer.GstLaunchExists)
@@ -4720,7 +4716,7 @@ namespace MissionPlanner.GCSViews
 
                 try
                 {
-                    hudGStreamer.StartA(url);
+                    hudGStreamer.Start(url);
                 }
                 catch (Exception ex)
                 {
